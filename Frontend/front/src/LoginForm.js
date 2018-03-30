d0d886e597d63506c3c1360828047eca9b4835e5
Brandon <brandondrice@aol.com>
2018-03-30 15:23:43 -0500

    Added google social login - React

--- Frontend/front/src/LoginForm.js
+++ Frontend/front/src/LoginForm.js
@@ -1,8 +1,73 @@
 import React, {Component} from 'react';
 import {Button, Container, Row, Col, Card, CardBody, Label, Input, Form, FormGroup} from 'reactstrap';
+import SocialLoginButtons from './SocialLoginButtons';
 
 
 class LoginForm extends Component {
+
+	constructor (props) {
+	    super(props)
+
+	    this.state = {
+	      logged: false,
+	      user: {},
+	      currentProvider: ''
+	    }
+	    this.nodes = {}
+
+	    this.onLoginSuccess = this.onLoginSuccess.bind(this)
+	    this.onLoginFailure = this.onLoginFailure.bind(this)
+	    this.onLogoutSuccess = this.onLogoutSuccess.bind(this)
+	    this.onLogoutFailure = this.onLogoutFailure.bind(this)
+	    this.logout = this.logout.bind(this)
+
+  	}
+  	setNodeRef (provider, node) {
+	    if (node) {
+	      this.nodes[ provider ] = node
+	    }
+	}
+
+  onLoginSuccess (user) {
+    console.log(user)
+
+    this.setState({
+      logged: true,
+      currentProvider: user._provider,
+      user
+    })
+  }
+
+  onLoginFailure (err) {
+    console.error(err)
+
+    this.setState({
+      logged: false,
+      currentProvider: '',
+      user: {}
+    })
+  }
+
+  onLogoutSuccess () {
+    this.setState({
+      logged: false,
+      currentProvider: '',
+      user: {}
+    })
+  }
+
+  onLogoutFailure (err) {
+    console.error(err)
+  }
+
+  logout () {
+    const { logged, currentProvider } = this.state
+
+    if (logged && currentProvider) {
+      this.nodes[currentProvider].props.triggerLogout()
+    }
+  }
+
 	render() {
 		return(
 			<Container className="py-5">
@@ -27,6 +92,7 @@ class LoginForm extends Component {
 			                                    <Input type="password" className="form-control-lg rounded-0" id="pwd1" required="" autoComplete="new-password"/>
 			                                    <div className="invalid-feedback">Enter your password too!</div>
 			                                </FormGroup>
+			                                
 			                                <div>
 			                                    <Label className="custom-control custom-checkbox">
 			                                      <Input type="checkbox" className="custom-control-input"/>
@@ -36,6 +102,14 @@ class LoginForm extends Component {
 			                                </div>
 			                                <Button type="submit" className="btn btn-success btn-lg float-right" id="btnLogin">Login</Button>
 			                            </Form>
+			                            <SocialLoginButtons
+									      provider='google'
+									      appId='102444846402-te4icsgjqam8ck7m90ig8ujrvj604s19.apps.googleusercontent.com'
+									      onLoginSuccess={this.onLoginSuccess}
+									      onLoginFailure={this.onLoginFailure}
+									    >
+									      Login with Google
+									    </SocialLoginButtons>
 			                        </CardBody>
 			                    </Card>         
 			                </div>
